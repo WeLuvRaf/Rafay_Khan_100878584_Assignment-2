baac6f4bf5aeae44dc526d3c2df3f47a687d775b
WeLuvRaf <rafay.khan1@ontariotechu.net>
2024-11-05 21:25:47 -0500

    html done

--- my-portfolio-app/routes/index.js
+++ my-portfolio-app/routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', body: '<h1>Welcome to My Portfolio</h1><p>This is the Home page</p>' });
+  res.render('index', { title: 'Home', body: '' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me', body: '<h1>About Me</h1><p>This is the About Me page.</p>' });
+  res.render('about', { title: 'About Me', body: '' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects', body: '<h1>My Projects</h1><p>This page showcases my projects.</p>' });
+  res.render('projects', { title: 'Projects', body: '' });
 });
 
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me', body: '<h1>Contact Me</h1><p>Feel free to reach out!</p>' });
+  res.render('contact', { title: 'Contact Me', body: '' });
 });
 
 module.exports = router;
