125376691d09e3f197f8318b62da09ccbc49d34f
WeLuvRaf <rafay.khan1@ontariotechu.net>
2024-11-05 20:27:12 -0500

    Set up everything now just make the website

--- my-portfolio-app/routes/index.js
+++ my-portfolio-app/routes/index.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', body: '<h1>Welcome to My Portfolio</h1><p>This is the Home page</p>' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About Me', body: '<h1>About Me</h1><p>This is the About Me page.</p>' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects', body: '<h1>My Projects</h1><p>This page showcases my projects.</p>' });
 });
 
+
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me', body: '<h1>Contact Me</h1><p>Feel free to reach out!</p>' });
 });
 
 module.exports = router;
